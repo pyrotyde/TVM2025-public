5a81a9937c4738220d1b2520dc1eb76fd4416f16
Evgenii Tcygankov <ertechak@yandex.ru>
2025-10-02 22:46:14 +0700

    lab03

--- lab03/src/calculate.ts
+++ lab03/src/calculate.ts
@@ -5,7 +5,54 @@ export const arithSemantics: ArithSemantics = grammar.createSemantics() as Arith
 
 
 const arithCalc = {
+    number(_) {
+        return parseInt(this.sourceString);
+    },
+    variable(_l, _r) {
+        return this.args.params[this.sourceString];
+    },
+    AddExpr_add(l, ops, rs) {
+        let res = l.calculate(this.args.params);
+        const n = ops.numChildren;
+        for (let i = 0; i < n; i++) {
+            switch (ops.child(i).sourceString) {
+                case '+': 
+                    res += rs.child(i).calculate(this.args.params);
+                    break;
+                case '-': 
+                    res -= rs.child(i).calculate(this.args.params);
+                    break;
+                default: throw new Error;
+            }
+        }
+        return res;
+    },
+    MulExpr_mult(l, ops, rs) {
+        let res = l.calculate(this.args.params);
+        const n = ops.numChildren;
+        for (let i = 0; i < n; i++) {
+            switch (ops.child(i).sourceString) {
+                case '*': 
+                    res *= rs.child(i).calculate(this.args.params);
+                    break;
+                case '/':
+                    let right = rs.child(i).calculate(this.args.params);
+                    if (right == 0) throw new Error("Division by zero");
+                    res /= right;
+                    break;
+                default: throw new Error;
+            }
+        }
+        return res;
+    },
+    ParExpr_pars(_lpar, expr, _rpar) {
+        return expr.calculate(this.args.params);
+    },
+    ParExpr_parUnary(_minus, expr) {
+        return -1 * expr.calculate(this.args.params);
+    }
 } satisfies ArithmeticActionDict<number | undefined>;
+
 
 
 arithSemantics.addOperation<Number>("calculate(params)", arithCalc);
